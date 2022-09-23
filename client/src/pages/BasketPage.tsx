71122b812873d65371fbdc8c677f227697483dec
Anton Karlsson <Anton7karlsson@gmail.com>
2022-09-23 13:34:03 +0200

    Uppdaterat BasketPage med kod från antd + mod lite

--- client/src/pages/BasketPage.tsx
+++ client/src/pages/BasketPage.tsx
@@ -1,19 +1,63 @@
 import React, { useEffect, useState } from "react";
+import { Table } from "antd";
+import * as FaIcons from "react-icons/fa";
 import agent from "../actions/agent";
 import { Basket } from "../models/basket";
-
+import { Course } from "../models/course";
 
 const BasketPage = () => {
-    const [items, setItems] = useState<Basket>()
+  const [items, setItems] = useState<Basket>();
 
-    useEffect(()=> {
-        agent.Baskets.get().then((response) => {
-            setItems(response);
-        })
-    }, [])
-    return <>
-    <h1>{items?.clientId}</h1> 
-    </>;
-}
+  useEffect(() => {
+    agent.Baskets.get().then((response) => {
+      setItems(response);
+    });
+  }, []);
+
+  const columns = [
+    {
+      title: "Image",
+      dataIndex: "image",
+      key: "image",
+      render: (url: string) => {
+        return <img width="100px" src={url} alt={url} />;
+      }
+    },
+    {
+      title: "Title",
+      dataIndex: "title",
+      key: "title",
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+      key: "price",
+    },
+    {
+      title: "Instructor",
+      key: "instructor",
+      dataIndex: "instructor",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (item: Course) => (
+        <div>
+            <FaIcons.FaTrash />
+        </div>
+      ),
+    },
+  ];
+  
+
+  <Table columns={columns} dataSource={items?.items} />;
+
+
+  return (
+    <>
+      <h1>{items?.clientId}</h1>
+    </>
+  );
+};
 
 export default BasketPage;
