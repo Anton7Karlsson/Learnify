3be864ba57dd23a63c1030df4e83efa534fb165c
Anton Karlsson <Anton7karlsson@gmail.com>
2022-09-25 21:43:22 +0200

    implementerat selectors

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect} from "react";
+import React, {useEffect} from "react";
 import { Course } from "../models/course";
-import agent from "../actions/agent";
-import { PaginatedCourse } from "../models/paginatedCourse";
 import ShowCourses from "../components/ShowCourses";
 import { Row } from "antd";
+import { useAppDispatch, useAppSelector } from "../redux/store/configureStore";
+import { coursesSelector, getCoursesAsync } from "../redux/slice/courseSlice";
 
 const Homepage = () => {
-  const [data, setData] = useState<PaginatedCourse>();
 
+const courses = useAppSelector(coursesSelector.selectAll);
+const dispatch = useAppDispatch();
+const {coursesLoaded} = useAppSelector(state => state.course);
 
   useEffect(() => {
-    agent.Courses.list().then((response) => {
-      setData(response);
-    });
-  }, []);
+    if(!coursesLoaded) dispatch(getCoursesAsync())
+  }, [coursesLoaded, dispatch]);
 
 
 
@@ -24,8 +24,8 @@ const Homepage = () => {
         <h2>New courses picked just for you..</h2>
       </div>
       <Row gutter={[24, 32]}>
-        {data &&
-          data.data.map((course: Course, index: number) => {
+        {courses &&
+          courses.map((course: Course, index: number) => {
             return <ShowCourses key={index} course={course}/>
           })}
       </Row>
--- client/src/redux/slice/courseSlice.ts
+++ client/src/redux/slice/courseSlice.ts
@@ -2,10 +2,11 @@ import { createAsyncThunk, createEntityAdapter, createSlice } from "@reduxjs/too
 import agent from "../../actions/agent";
 import { Course } from "../../models/course";
 import { PaginatedCourse } from "../../models/paginatedCourse";
+import { RootState } from "../store/configureStore";
 
 const coursesAdapter = createEntityAdapter<Course>();
 
-const getCoursesAsync = createAsyncThunk<PaginatedCourse | undefined, void>(
+export const getCoursesAsync = createAsyncThunk<PaginatedCourse | undefined, void>(
     "course/getCoursesAsync", async () => {
         try{
             return await agent.Courses.list();
@@ -29,10 +30,13 @@ const getCoursesAsync = createAsyncThunk<PaginatedCourse | undefined, void>(
         });
         builder.addCase(getCoursesAsync.fulfilled, (state, action) => {
             coursesAdapter.setMany(state, action.payload!.data);
-            state.status = "idle"
+            state.status = "idle";
+            state.coursesLoaded = true;
         });
         builder.addCase(getCoursesAsync.rejected, (state) => {
             state.status = "idle"
         });
     }
- })
+ });
+
+ export const coursesSelector = coursesAdapter.getSelectors((state: RootState) => state.course);
