b0f53eb0e6090bd9f5bf15b306cd44612581a54e
Anton Karlsson <Anton7karlsson@gmail.com>
2022-09-25 18:33:54 +0200

    import basketslice/ missades i förra push

--- client/src/redux/store/configureStore.ts
+++ client/src/redux/store/configureStore.ts
@@ -1,5 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+import { basketSlice } from "../slice/basketSlice";
 import { loginSlice } from "../slice/loginSlice";
 
     export const store = configureStore({
